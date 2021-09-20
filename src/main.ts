93070ba99b19d35cf4e59f3f82e04d4e424bb0c1
RacconWebFenix <dfenixweb@gmail.com>
2021-09-20 07:41:29 -0300

    Mudanca de porta

--- src/main.ts
+++ src/main.ts
@@ -16,6 +16,6 @@ async function bootstrap() {
 
   app.useGlobalInterceptors(new UnauthorizedInterceptor());
 
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
